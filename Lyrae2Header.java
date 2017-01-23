0f03edd3cd809b92a2598c3ca2857d0ba6a59c03
khadija asafar <khadija.asafar@edu.uca.ma>
2017-01-23 11:02:43 +0000

    Lyrae2Header.java

---
+++ Lyrae2Header.java
@@ -0,0 +1,10 @@
+
+package javaapplication1;
+
+interface Lyrae2Header {
+    int N_COLS=256;
+    int nPARALLEL=2;
+    int BLOCKS_LEN_BYTES = /*Byte.SIZE */ 96;
+    int ROW_LEN_BYTES= 64* BLOCKS_LEN_BYTES;
+    
+}
