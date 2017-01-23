14d7e24e1233e60b9134ff28f2c811a1131ca3c1
SouhailaMGD <souhailamerghad@gmail.com>
2017-01-23 14:50:33 +0000

    file.java

---
+++ file.java
@@ -0,0 +1,13 @@
+package projet;
+
+public class Projet {
+  
+ public static void main(String[] argv) {
+    Object a=new String("strstr");
+    if(a instanceof String)
+    {String str = (String) a;
+    System.out.println(str);}
+    
+}
+ 
+}
